7de4dae6a3aa713eb9006706c33786cc8611b020
Gaetan <gaetan@trivino.fr>
2012-12-13 21:20:23 +0100

    listing templates

--- login.php
+++ login.php
@@ -3,20 +3,17 @@
 error_reporting(E_ALL);
 include("pve2_api.class.php");
 
-$pve2 = new PVE2_API("10.10.10.1", "demo-vps", "pve", "Bonjour");
-# realm above can be pve, pam or any other realm available.
-if ($pve2->constructor_success()) {
-    /* Optional - enable debugging. It print()'s any results currently */
-    // $pve2->set_debug(true);
-
-    if ($pve2->login()) {
-	echo "OK. Listing nodes\n";
-      foreach ($pve2->get_node_list() as $node_name) {
-            print_r($pve2->get("/nodes/".$node_name."/status"));
-        }
+$pveuser = new PVE2_API("10.10.10.1", "demo-vps", "pve", "Bonjour");
+$pveroot = new PVE2_API("10.10.10.1", "root", "pve", "Bonjour");
+if ($pveuser->constructor_success() && $pveroot->constructor_success()) {
+    if ($pveuser->login() && $pveroot->login()) {
+		echo "OK. Listing OpenVZ\n";
+		print_r($pveuser->get("/nodes/ns212937/openvz"));	
+		echo "OK. Listing template</br>\n";
+		print_r($pveroot->get("/nodes/ns212937/storage/local/content/templates"));					 
     } else {
         print("Login to Proxmox Host failed.\n");
-        exit;
+		exit;
     }
 } else {
     print("Could not create PVE2_API object.\n");
